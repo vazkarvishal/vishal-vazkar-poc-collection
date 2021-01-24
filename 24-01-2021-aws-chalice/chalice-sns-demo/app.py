16d27e48bb0db8a6567e0695a2d72bd96194d6a7
vishal.v@mmtdigital.co.uk <vishal.v@mmtdigital.co.uk>
2021-01-24 18:03:48 +0000

    chalice sns demo app

---
+++ 24-01-2021-aws-chalice/chalice-sns-demo/app.py
@@ -0,0 +1,36 @@
+from chalice import Chalice
+import requests
+
+app = Chalice(app_name='chalice-sns-demo')
+
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+@app.on_sns_message(topic='chaliceDemoTopic')
+def handle_sns_message(event):
+    app.log.debug("Received message with subject: %s, message: %s",
+                    event.subject, event.message)
+    # response = requests.post('your_slac_hook', data='{"text":"Hello, World!"}')
+    # app.log.info(response)
+    
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
