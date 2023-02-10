47ef6fd571dce5f6260f349f9e1cb3765259923f
Vishal Vazkar <vishal.vazkar@MMT-MAC-4236.local>
2023-02-10 06:01:54 +0000

    istio auto injection

--- 01-02-2023-istio-with-eks/terraspace/app/stacks/istio/main.tf
+++ 01-02-2023-istio-with-eks/terraspace/app/stacks/istio/main.tf
@@ -1,98 +1,92 @@
-# Installs CRDs needed by istio to run
 resource "helm_release" "istio_base" {
-  name       = "istio-base"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "base"
-  version    = "v1.16.2"
-
   namespace        = "istio-system"
   create_namespace = true
-  timeout          = 180
-  force_update     = true
-  recreate_pods    = true
-  max_history      = 10
-  wait             = true
-  wait_for_jobs    = true
-  lint             = true
+  name             = "istio-base"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "base"
+  version          = "v1.16.2"
+  timeout          = 120
   cleanup_on_fail  = true
-
+  wait             = true
+  lint             = true
 }
 
-
-# Installs istio daemon
 resource "helm_release" "istiod" {
-  name       = "istiod"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "istiod"
-  version    = "v1.16.2"
-  namespace  = "istio-system"
-
-  create_namespace = true
-  timeout          = 180
-  force_update     = true
-  recreate_pods    = true
-  max_history      = 10
-  wait             = true
-  wait_for_jobs    = true
-  lint             = true
-  cleanup_on_fail  = true
-  set {
-    name  = "meshConfig.accessLogFile"
-    value = "/dev/stdout"
-  }
-
-  depends_on = [helm_release.istio_base]
-}
-
-# Creates istio-ingress service that controls traffic coming inside the mesh
-resource "helm_release" "istio_gateway_deployment" {
-  name       = "istio-ingress-gateway-deployment"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "gateway"
-  version    = "v1.16.2"
-
   namespace        = "istio-system"
   create_namespace = true
-  timeout          = 180
-  force_update     = true
-  recreate_pods    = true
-  max_history      = 10
-  wait             = true
-  wait_for_jobs    = true
-  lint             = true
+  name             = "istiod"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "istiod"
+  version          = "v1.16.2"
+  timeout          = 120
   cleanup_on_fail  = true
-  depends_on       = [helm_release.istiod]
+  wait             = true
+  lint             = true
+  set {
+    name  = "sidecarInjectorWebhook.enableNamespacesByDefault"
+    value = "true"
+  }
+  depends_on = [
+    helm_release.istio_base
+  ]
+}
 
+resource "kubectl_manifest" "istio_gateway_namespace" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: istio-ingressgateway
+      labels:
+        istio-injection: enabled
+  YAML
+  depends_on = [
+    helm_release.istiod
+  ]
+}
+
+resource "helm_release" "istio_ingress_gateway" {
+  namespace        = kubectl_manifest.istio_gateway_namespace.namespace
+  create_namespace = true
+  name             = "istio-ingressgateway"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "gateway"
+  version          = "v1.16.2"
+  timeout          = 120
+  cleanup_on_fail  = true
+  max_history      = 5
+  wait             = true
+  lint             = true
   set {
     name  = "service.type"
     value = "NodePort"
   }
-
+  depends_on = [kubectl_manifest.istio_gateway_namespace]
 }
 
-resource "kubectl_manifest" "istio_gateway_resource-pip" {
-  yaml_body = <<-YAML
-    apiVersion: networking.istio.io/v1alpha3
-    kind: Gateway
-    metadata:
-      name: pip
-      namespace: istio-system
-    spec:
-      selector:
-        istio: ingress-gateway-deployment   # This should match the istio: <istio-ingress-gateway-name> label
-      servers:
-        - port:
-            number: 80
-            name: http
-            protocol: HTTP
-          hosts:
-            - "*"
-  YAML
+# resource "kubectl_manifest" "istio_gateway_resource-pip" {
+#   yaml_body = <<-YAML
+#     apiVersion: networking.istio.io/v1alpha3
+#     kind: Gateway
+#     metadata:
+#       name: pip
+#       namespace: istio-system
+#     spec:
+#       selector:
+#         istio: ingress-gateway-deployment   # This should match the istio: <istio-ingress-gateway-name> label
+#       servers:
+#         - port:
+#             number: 80
+#             name: http
+#             protocol: HTTP
+#           hosts:
+#             - "*"
+#   YAML
 
-  depends_on = [
-    helm_release.istio_gateway_deployment
-  ]
-}
+#   depends_on = [
+#     helm_release.istio_gateway_deployment
+#   ]
+# }
 
 # resource "helm_release" "kiali_operator_deployment" {
 #   name       = "kiali-operator-deployment"
